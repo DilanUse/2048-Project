2572c839e657b4e19d746a0e219c2403de0e12f8
DilanUse <dilan_8-10@hotmail.com>
2016-08-15 23:31:58 -0400

    Puntuaciones del Juego
    Se realizo la escena para las 10 mejores puntuaciones del juego

---
+++ 2048(Unity Project)/Assets/Scripts/Puntuacion.cs
@@ -0,0 +1,29 @@
+﻿
+/**
+ * Representación de una puntuación
+ */
+public class Puntuacion
+{
+    public string nombre; // nombre para el puntaje
+    public int puntuacion; // puntacion para el puntaje
+
+
+    // contructor predeterminado
+    public Puntuacion() : this( null, 0 ){}
+
+
+    // contructor paramterico
+    public Puntuacion( string nombre, int puntuacion )
+    {
+        this.nombre = nombre;
+        this.puntuacion = 0;
+    } // fin del constructor 
+
+
+    // retorna una representacion de una puntuacion en la forma
+    // nombre;puntuacion
+    public override string ToString()
+    {
+        return this.nombre + ";" + this.puntuacion;
+    } // fin de toString
+} // fin de puntuacion;
---
+++ 2048(Unity Project)/Assets/Scripts/Puntuaciones.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.IO;
+using System;
+using System.Linq;
+
+
+/**
+ * Actualiza las 10 maximas puntuaciones 
+ */
+public class Puntuaciones : MonoBehaviour
+{
+    public Text nombres; // campo de texto de los nombres
+    public Text puntos; // campo de testo de los puntos
+    Puntuacion[] registros; // registros de puntajes
+
+
+
+    // muestra los puntajes al entrar en maximas puintuaciones
+    void Start()
+    {
+        this.LeerPuntaje();
+
+
+        nombres.text = puntos.text = "";
+
+
+        for (int i = 1; i < registros.Length; i++)
+        {
+            nombres.text += i + ".-" +  ( registros[i].nombre != null ? registros[i].nombre : "AAAAA" )
+                + System.Environment.NewLine;
+            puntos.text += registros[i].puntuacion + System.Environment.NewLine;
+        } // fin del for
+    } // fin de estar
+
+
+    // lee los puntajes alamcenados en disco
+    private void LeerPuntaje()
+    {
+        FileStream puntajes; // archivo
+        StreamReader puntajes_in; // flujo de entrada
+        string[] buffer; // lineas del archivo
+        char[] separador = { ';' }; // delimitador
+
+
+
+        try
+        {
+            puntajes = new FileStream("Assets/Save/Puntajes.txt", FileMode.Open, FileAccess.Read);
+
+           
+            using ( puntajes_in = new StreamReader(puntajes) )
+            {
+                registros = new Puntuacion[11];
+
+
+                
+                for ( int i = 0; i < registros.Length ; ++i )
+                {
+                    if( !puntajes_in.EndOfStream )
+                    {
+                        buffer = puntajes_in.ReadLine().Split(separador);
+                        registros[i] = new Puntuacion( buffer[0], Convert.ToInt32(buffer[1]));
+                    }
+                    else
+                        registros[i] = new Puntuacion();
+                } // fin del for
+            } // fin del using
+        } // fin del try
+        catch ( IOException e )
+        {
+            Debug.Log(e.Message);
+        } // fin del try...catch
+    } // fin de leer 
+
+
+
+    // actualiza los puntajes ingresando uno nuevo y ordenandolos
+    public void actualizarPuntajes( Puntuacion nuevoPT )
+    {
+        FileStream puntajes; //archivo
+        StreamWriter puntajes_out; // flujo de salida
+        int registro = 0;
+
+
+
+        this.LeerPuntaje();
+        registros[11] = nuevoPT;
+
+
+        try
+        {
+            puntajes = new FileStream("Assets/Save/Puntajes.txt", FileMode.Truncate, FileAccess.Write);
+
+
+            using (puntajes_out = new StreamWriter(puntajes))
+            {
+                var PuntajesOrdenados =
+                    from puntaje in registros
+                    orderby puntaje.puntuacion descending
+                    select puntaje;
+
+
+                foreach (var puntaje in PuntajesOrdenados )
+                {
+                    if (registro >= 10)
+                        break;
+
+
+                    puntajes_out.WriteLine(puntaje);
+                    ++registro;
+                } // fin del foreach
+            } // fin del using
+        } // fin del try
+        catch (IOException e)
+        {
+            Debug.Log(e.Message);
+        } // fin del try...catch
+    } // fin de actualizarPuntajes
+} // fin de Puntuaciones 
